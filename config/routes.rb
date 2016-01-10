364271174d79a3a183aacd57e85a2d399e35dce7
Carl Weis <carl@carlweis.com>
2016-01-10 13:59:19 -0600

    Update routes file formatting

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # API definition
-  namespace :api, defaults: {format: :json},
-                            constraints: {subdomain: 'api'}, path: '/' do
+  namespace :api, defaults: { format: :json },
+                            constraints: { subdomain: 'api' }, path: '/' do
     # We are going to lis tour resources here
   end
 end
